5204d13abf58896dfbdc2ae3fd640ccfc7c4622a
Pete <pete.zink03@gmail.com>
2024-03-21 13:29:07 -0400

    Added Shopping cart Icon and Sheet functionality from Shadcn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { MaxWidthScreen } from "./MaxWidthScreen";
 import { Icons } from "./Icons";
 import { NavItems } from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import { Cart } from "./Cart";
 
 export function Navbar(){
     const user = null
@@ -25,8 +27,31 @@ export function Navbar(){
                         <div className="ml-auto flex items-center">
                             <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                                 {user ? null : (
-                                    <Link href='/sign-in'>Sign in</Link>
+                                    <Link className={buttonVariants({variant: "ghost"})} href='/sign-in'>Sign in</Link>
                                 )}
+                                {user ? null : (
+                                    <span className="h-6 w-px bg-gray-200" />
+                                )}
+
+                                {user ? (
+                                <p></p>
+                                ) : (
+                                <Link
+                                href='/sign-up'
+                                className={buttonVariants({variant: "ghost"})}>
+                                    Create Account
+                                </Link>
+                                )}
+                                {user ? <span className="h-6 w-px bg-gray-200" /> : null}
+
+                                {user ? null : (
+                                    <div className="flex lg:ml-6">
+                                        <span className="h-6 w-px bg-gray-200" />
+                                    </div>
+                                )}
+                                <div className="ml-4 flow-root lg:ml-6">
+                                    <Cart />
+                                </div>
                             </div>
                         </div>
                         </div>
