4f00217c6b518a4011ddc58452c27cd17f102573
Pete <pete.zink03@gmail.com>
2024-03-21 14:33:59 -0400

    Added format currency function to adjust currency inputs

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,8 +2,13 @@
 
 import { ShoppingCart } from "lucide-react"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Separator } from "./ui/separator"
+import { formatPrice } from "@/lib/utils"
 
 export function Cart(){
+    const itemCount = 1
+
+    const fee = 1
     return (
         <Sheet>
             <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -16,7 +21,44 @@ export function Cart(){
                 <SheetHeader className="space-y-2.5 pr-6">
                     <SheetTitle>Cart (0)</SheetTitle>
                 </SheetHeader>
-
+                {itemCount > 0 ? (
+                    <>
+                        <div className="flex flex-col w-full pr-6">
+                            cart items
+                        </div>
+                        <div className="space-y-4 pr-6">
+                            <Separator />
+                            <div className="space-y-1.5 text-sm">
+                                <div className="flex">
+                                    <span className="flex-1">
+                                        Shipping
+                                    </span>
+                                    <span>
+                                        Free
+                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <span className="flex-1">
+                                        Transaction Fee
+                                    </span>
+                                    <span>
+                                        {formatPrice(fee)}
+                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <span className="flex-1">
+                                        Total
+                                    </span>
+                                    <span>
+                                        {formatPrice(fee)}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div></div>
+                )}
             </SheetContent>
         </Sheet>
     )
