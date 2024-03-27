6b3288a2ccae00f960e9c77aa150624613073512
Pete <pete.zink03@gmail.com>
2024-03-27 15:14:41 -0400

    Finished styling the shopping cart. Style is dynamic depending on if there are items in the cart.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import { ShoppingCart } from "lucide-react"
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
 import { Separator } from "./ui/separator"
 import { formatPrice } from "@/lib/utils"
+import { buttonVariants } from "./ui/button"
+import Link from "next/link"
+import Image from "next/image"
 
 export function Cart(){
-    const itemCount = 1
+    const itemCount = 0
 
     const fee = 1
     return (
@@ -54,10 +57,38 @@ export function Cart(){
                                     </span>
                                 </div>
                             </div>
+                            <SheetFooter>
+                                <SheetTrigger asChild>
+                                    <Link href='/cart' className={buttonVariants({
+                                        className: 'w-full',
+                                    })}>
+                                        Continue to Checkout
+                                    </Link>
+                                </SheetTrigger>
+                            </SheetFooter>
                         </div>
                     </>
                 ) : (
-                    <div></div>
+                    <div className="flex h-full flex-col items-center justify-center space-y-1">
+                        <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+                            <Image src='/assets/images/hippo-empty-cart.png' fill alt='empty shopping cart'/>
+                        </div>
+                        <div className="text-xl font-semibold">
+                            Your cart is empty
+                        </div>
+                        <SheetTrigger>
+                            <Link
+                            href='/products'
+                            className={buttonVariants({
+                                variant: 'link',
+                                size: 'sm',
+                                className: 'text-sm text-muted-foreground'
+                            })}
+                            >
+                                Add items to your cart to checkout
+                            </Link>
+                        </SheetTrigger>
+                    </div>
                 )}
             </SheetContent>
         </Sheet>
